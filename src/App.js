f24ce777ea1facd7bb0fb8a839a0c07d0e3629c8
misun <eating8@naver.com>
2021-11-16 16:34:08 +0900

    개발환경 세팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          안녕하세요?
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
