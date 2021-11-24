ca98f4d8cc118e16341fc26cda9d706dab0d2ce0
misun <eating8@naver.com>
2021-11-24 16:50:56 +0900

    Button에 필요없는 변수 삭제

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,12 +3,6 @@ import "../css/Button.css";
 
 // 버튼
 export function Button(props) {
-  const [name, setName] = useState("");
-  const [gender, setGender] = useState("");
-
-  useEffect(() => setName(props.name), [props.name]);
-  useEffect(() => setGender(props.gender), [props.gender]);
-
   return (
     <button className={props.checked ? "btn_click" : "btn"}>{props.text}</button>
   );
--- src/components/Example.js
+++ src/components/Example.js
@@ -19,7 +19,7 @@ export function Example() {
       <Progressbar text="검사예시" percent="0" />
       <div className="explanation">
         <p>직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에 표시하세요.</p>
-        <p>가치의 뜻을 잘 모르겠다면 문항 아래에 있는 가치의 설명을 확인해보세요.</p>
+        <p>가치의 뜻을 잘 모르겠다면 문항에 마우스 커서를 올려서 설명을 확인해보세요.</p>
       </div>
       <CheckBox onClick={handleClick} name="ex" 
       answer01="능력발휘" 
