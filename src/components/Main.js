6b96478ef475b3ba0afb5113b110d6266d8ccd3d
misun <mcoding777@naver.com>
2021-12-01 23:51:25 +0900

    유저 정보(이름, 성별)를 localstorage로 저장

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,11 +17,13 @@ export function Main(props) {
   // input에 들어온 이름으로 바꾸는 함수
   function handleChange(event) {
     setName(event.target.value);
+    localStorage.setItem("user_name", event.target.value);
     console.log("이름 바꿨당");
   }
 
   function getGender(event) {
     setGender(event.target.value);
+    localStorage.setItem("user_gender", event.target.value);
     console.log("성별 바꿨당");
   }
 
