2a8dbff5b182609e14692ea029a163de3096bc5a
misun <mcoding777@naver.com>
2022-01-12 21:30:16 +0900

    styled-components 설치 및 App에 전역 스타일링 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, } from "react";
+import React from "react";
 import { Main } from './components/Main';
 import { Example } from './components/Example';
 import { Test } from './components/Test';
@@ -8,12 +8,14 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   console.log("App 컴포넌트가 렌더링 됐습니다.");
   
   return (
     <>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/example" element={<Example />} />
@@ -26,3 +28,14 @@ function App() {
 }
 
 export default App;
+
+// styled-components
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {   
+    text-align: center;
+  }
+`;
