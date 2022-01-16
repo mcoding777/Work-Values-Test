af65495095ae566b81545c51f1c702f4e153f085
misun <mcoding777@naver.com>
2022-01-17 01:09:35 +0900

    배포 오류 : 갑자기 또 안된다... 왜? basename 때문???

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ let store = createStore(reducer, composeWithDevTools());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
